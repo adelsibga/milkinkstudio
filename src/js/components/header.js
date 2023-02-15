3f18472dfde5790602b11631712e8719880bca97
Adel Sibgatullin <adelsibga@gmail.com>
2023-02-16 01:13:30 +0300

    Убрал навигацию со страницы и добавил ее в меню Добавил страницу privacy-policy Правки по коду Добавил анимации

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,6 +1,39 @@
+const close = document.querySelector('.close')
 const burger = document.querySelector('.js-burger')
 const burgerMenu = document.querySelector('.js-burger-wrapper')
+const duration = 600
 
-burger.addEventListener('click', () => {
-    console.log(burgerMenu)
-})
+burger.onclick = () => {
+    openMenu()
+}
+
+close.onclick = () => {
+    closeMenu()
+}
+
+document.onkeydown = (e) => {
+    if (e.code === 'Escape') {
+        closeMenu()
+    }
+}
+
+const closeMenu = () => {
+    burgerMenu.classList.remove('fadeInUp')
+    burgerMenu.classList.add('reverse')
+    burgerMenu.classList.add('slideInUp')
+    setTimeout(() => {
+        document.body.style.overflow = ''
+        burgerMenu.classList.add('hidden')
+        burgerMenu.classList.remove('reverse')
+        burgerMenu.classList.remove('slideInUp')
+    }, duration)
+}
+
+const openMenu = () => {
+    document.body.style.overflow = 'hidden'
+    burgerMenu.classList.add('fadeInUp')
+    burgerMenu.classList.remove('hidden')
+    setTimeout(() => {
+        burgerMenu.classList.remove('fadeInUp')
+    }, duration)
+}
